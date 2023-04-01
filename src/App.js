0ba74eb332053dea30db29fd43c85eef634ceab8
mit-anuj <anujmittal0040@gmail.com>
2023-04-01 20:32:07 +0530

    created a new About.js file and created a button to enable and disable dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
+import About from './Components/About';
 
 function App() {
   return (
     <>
       <Navbar title="hello" aboutText="About" />
       <div className='container my-3' >
-        <TextForm heading="Enter the text to analyze" />
+        {/* <TextForm heading="Enter the text to analyze" /> */}
       </div>
+      <About />
     </>
   );
 }
